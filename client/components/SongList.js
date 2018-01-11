a81fdf04ea5b1255edd59b2c7882a8f0348b9156
Jon Huh <oneandwholly@gmail.com>
2018-01-11 12:33:59 -0800

    add Delete button for songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -5,11 +5,19 @@ import { Link } from 'react-router-dom';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+    onSongDelete(id) {
+        this.props.mutate({ variables: { id }})
+            .then(() => this.props.data.refetch());
+    }
+    
     renderSongs() {
         return this.props.data.songs.map(
-            song => 
-                <li key={song.id} className="collection-item">
-                    {song.title}
+            ({ id, title }) => 
+                <li key={id} className="collection-item">
+                    {title}
+                    <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+                        delete
+                    </i>
                 </li>
         );
     }
@@ -35,4 +43,15 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+    mutation DeleteSong($id: ID) {
+        deleteSong(id: $id) {
+            id
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(
+    graphql(query)(SongList)
+);
