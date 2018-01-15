f7a0f623e127a5130365b9663aa140ac6c30e147
Jon Huh <oneandwholly@gmail.com>
2018-01-15 00:50:04 -0800

    add routing for SongDetail component and fetch song

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -14,7 +14,9 @@ class SongList extends Component {
         return this.props.data.songs.map(
             ({ id, title }) => 
                 <li key={id} className="collection-item">
+                    <Link to={`/songs/${id}`} style={{textDecoration: 'none', color: 'inherit'}}>
                     {title}
+                    </Link>
                     <i className="material-icons" onClick={() => this.onSongDelete(id)}>
                         delete
                     </i>
--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
 
 const client = new ApolloClient({});
 
@@ -16,8 +17,11 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router>
         <App>
-          <Route exact path="/" component={SongList} />
-          <Route path="/songs/new" component={SongCreate} />
+          <Switch>
+            <Route exact path='/' component={SongList} />
+            <Route path='/songs/new' component={SongCreate} />
+            <Route path='/songs/:id' component={SongDetail} />
+          </Switch>
         </App>
       </Router>
     </ApolloProvider>
